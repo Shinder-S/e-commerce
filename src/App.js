a6ba2e45d86ceb79e9c1f73e11c3cfc803442e9e
santiagoshinder <santiagoshinder@gmail.com>
2024-03-26 14:49:35 -0300

    Add folders to give structure to the development, also add the part of new offers and the comparison with the old prices

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from  'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/mens" element={<ShopCategory category="Men"/>}/>
+        <Route path="/kids" element={<ShopCategory category="Kid"/>}/>
+        <Route path="/womens" element={<ShopCategory category="Women"/>}/>
+        <Route path="product" element={<Product/>}>
+          <Route path='productId' element={<Product/>}/>
+        </Route>
+        <Route  path="/cart" element={<Cart/>}/>
+        <Route  path="/login" element={<LoginSignup/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
